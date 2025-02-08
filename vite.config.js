800750a4df080d0bfab2a91c523cf674b120e893
MeviDiRaizel <mevidiraizel@gmail.com>
2025-02-08 20:03:36 +0800

    Update vite.config.js with base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-base: '/Portfolio/',
-  server: {
-    host: true, // or '0.0.0.0'
-  },
-})
+    plugins: [react()],
+    base: '/Portfolio/', // Add this line
+    server: {
+      host: true, // or '0.0.0.0'
+    },
+  })
