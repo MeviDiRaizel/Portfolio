bc8a51ba48e27d95cf6788bd90adb447ad2e61f6
MeviDiRaizel <mevidiraizel@gmail.com>
2025-02-08 20:11:53 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Portfolio/', // Add this line
+    base: '/Portfolio/', 
     server: {
-      host: true, // or '0.0.0.0'
+      host: true, 
     },
   })
